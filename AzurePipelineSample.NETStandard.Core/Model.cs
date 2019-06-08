e5caefaf886351971e4bc471c6f6deaa89394abf
meil <ics.sora.meil@gmail.com>
2019-06-09 03:59:50 +0900

    Update Model.cs

--- AzurePipelineSample.NETStandard.Core/Model.cs
+++ AzurePipelineSample.NETStandard.Core/Model.cs
@@ -4,7 +4,7 @@ namespace AzurePipelineSample.NETStandard.Core
 {
     public class Model : IModel
     {
-        // comment 4
+        // comment 5
         private readonly ILibrary library;
 
         public string Name { get; }
